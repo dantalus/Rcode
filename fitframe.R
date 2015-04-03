ba655e3f3c7d6a75913837a61ca2c62efed34703
Darren Dahly <ddahly@ucc.ie>
2015-04-04 00:17:24 +0100

    add plots for fit and entropy

--- fitframe.R
+++ fitframe.R
@@ -1,13 +1,13 @@
 fitframe <- function(target = getwd()) {
     
     # Turns Mplus .out files into a dataframe containing the AIC, BIC, ssaBIC, 
-    # Entropy, and Log-Likelihood for each model.
+    # Entropy, and Log-Likelihood for each model. 
     #
     # Arguments:
     #
-    # target: a folder containing the .out files. Defaults to the current
-    #         working directory, while target = "subfolder" will point to a the
-    #         named subfolder in your working directory. 
+    # target - a folder containing the .out files. Defaults to the current
+    #          working directory, while target = "subfolder" will point to a the
+    #          named subfolder in your working directory. 
     # 
     # Returns: 
     #
@@ -39,3 +39,39 @@ fitframe <- function(target = getwd()) {
     return(fit)
     
   }  
+
+fitPlot <- function(data) {
+    
+               require(ggplot2)
+    
+               return(ggplot(subset(data, model > 0 & stat < 4), 
+                             aes(x = model, y = value, shape = stat2)) +                
+                      geom_point(size = 4) +                               
+                      geom_line() +                                        
+                      ylab("AIC/BIC/aBIC") +
+                      xlab("Number of Profiles in the Model") +
+                      scale_x_continuous(breaks = c(1:5)) +                          
+                      theme(text = element_text (color = "black"), 
+                            strip.background = element_blank(),
+                            panel.grid.minor = element_blank(),
+                            legend.position = "bottom", 
+                            legend.title = element_blank()))
+            }
+  
+  entPlot <- function(data) {
+    
+               require(ggplot2)
+    
+               return(ggplot(subset(data, model > 0 & stat == 4), 
+                             aes(x = model, y = value, shape = stat2)) +                
+                        geom_point(size = 4) +                               
+                        geom_line() +                                        
+                        ylab("Entropy") +
+                        xlab("Number of Profiles in the Model") +
+                        scale_x_continuous(breaks = c(2:5)) +   
+                        scale_y_continuous(limits = c(0.5, 1), breaks = seq(0.5, 1, .05)) + 
+                        theme(text = element_text (color = "black"), 
+                              panel.grid.minor = element_blank())
+                              legend.position = "bottom", 
+                              legend.title = element_blank()))
+             }
